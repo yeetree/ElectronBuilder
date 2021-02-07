e284246fae85706b5950001311505dd584382303
Evan Viele <vinnyrulesgaming@gmail.com>
2021-02-06 23:03:40 -0600

    update 1.0.1

---
+++ src/js/unused/posts.js
@@ -0,0 +1,14 @@
+var newsDiv = document.getElementById("newsDiv");
+
+var newsSrc = "https://yeetree.github.io/ElectronBuilderSite/news-sys/";
+
+//a list of posts to be shown in the NEWS section
+//place newer posts in the front
+var postList = ["2-6-2021-3.html", "2-6-2021-2.html", "2-6-2021.html"];
+
+var i;
+function uploadPosts() {
+  for (i = 0; i <postList.length; i++) {
+    newsDiv.innerHTML += "<iframe src='" + newsSrc + postList[i] + "' onload='javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+`px`;}(this));' style='height:200px;width:100%;border:none;overflow:hidden;'</iframe>"
+  }
+}
